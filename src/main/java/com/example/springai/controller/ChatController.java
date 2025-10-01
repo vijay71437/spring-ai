04d160f46085dc47d7f197f1896099ee977062c8
vijay714371 <vijaymcse2024@gmail.com>
2025-10-01 14:43:29 +0530

    Your commit message

---
+++ src/main/java/com/example/springai/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.example.springai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }
+    @GetMapping("/chat1")
+    public String chat(@RequestParam String masseges) {
+        return chatClient.prompt().user(masseges).call().content();
+    }
+}
